3812a4be745d8faf2ca4c0ba19afdde4d95c092f
NizanMandelblit <nizanm91@gmail.com>
2019-12-17 23:31:23 +0200

    inital working lexer function

--- main.cpp
+++ main.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <vector>
+#include<fstream>
 
 using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+vector<string> lexer(char *string);
+
+int main(int argc, char *argv[]) {
+    lexer(argv[1]);
     return 0;
 }
 
-
-vector<string> Lexer(FILE filename) {
-    
-
-
+vector<string> lexer(char *filename) {
+    vector<string> vector;
+    ifstream file;
+    file.open(filename);
+    int indexVector = 0;
+    for (string line; getline(file, line);) {
+        int startString = 0;
+        int endString = 0;
+        for (char &c: line) {
+            if (c == ' ' || c == '(') {
+                break;
+            }
+            endString++;
+        }
+        vector.insert(vector.begin() + indexVector, line.substr(startString, endString));
+        startString = endString;
+        indexVector++;
+    }
 }
+
