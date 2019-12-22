b669c22e3b32b128d4b51a8329ddfaaa3ab7be15
NizanMandelblit <nizanm91@gmail.com>
2019-12-22 23:59:02 +0200

    Merge branch 'parser' of https://github.com/NizanMandelblit/ex3
     Conflicts:
    	CMakeLists.txt
    	OpenServerCommand.cpp

--- Print.cpp
+++ Print.cpp
@@ -2,8 +2,27 @@
 // Created by nizan on 22/12/2019.
 //
 
+#include <iostream>
 #include "Print.h"
 
 int Print::execute(vector<string> v, int index) {
-    return 0;
+    int flag = 0;
+    string temp = "";
+    if (v[index + 1][0] == '\c') {
+        for (char c: v[index + 1]) {
+            if (c == '"') {
+                flag = 1;
+                temp += c;
+                if (flag == 1 && c == '"') {
+                    cout << temp;
+                    flag = 0;
+                    temp = "";
+                }
+            }
+        }
+    } else {
+        cout << varsMap[v[index + 1]];<<endl
+    }
+
+
 }
